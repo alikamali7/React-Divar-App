4b7ea0c3b01d88758f9d86ebef81531d6bb3783d
Ali <alikamali0328@gmail.com>
2025-02-25 20:00:10 +0330

    Step 4: Configuring the react query and getting user data and setting the access token in the header using the interceptor and getting a new access token using the refresh token.

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import PageNotFound from "pages/404";
 import AdminPage from "pages/AdminPage";
 import AuthPage from "pages/AuthPage";
 import DashboardPage from "pages/DashboardPage";
 import HomePage from "pages/HomePage";
+import { getProfile } from "services/user";
 
 function Router() {
+  const { data, isLoading, error } = useQuery(["profile"], getProfile);
+  console.log({ data, isLoading, error });
+
+  if (isLoading) return <h1>Loading...</h1>
   return (
     <Routes>
       <Route index element={<HomePage />} />
