32cb69ef1a87fb2d43634ae9dc2908d2c700a1c9
Ali <alikamali0328@gmail.com>
2025-02-28 20:48:33 +0330

    Step 11: Implementing the site's home page, sidebar, and categories, and getting and displaying a list of all ads, and adding CSS to the home page.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import Main from "components/templates/Main";
+import Sidebar from "components/templates/Sidebar";
+import Loader from "components/modules/Loader";
+import { getAllPosts } from "services/user";
+import { getCategory } from "services/admin";
+
+const style = { display: "flex" };
+
 function HomePage() {
-  return <div>HomePage</div>;
+  const { data: posts, isLoading: postLoading } = useQuery(
+    ["post-list"],
+    getAllPosts
+  );
+  const { data: categories, isLoading: categoryLoading } = useQuery(
+    ["get-categories"],
+    getCategory
+  );
+
+  return (
+    <>
+      {postLoading || categoryLoading ? (
+        <Loader />
+      ) : (
+        <div style={style}>
+          <Sidebar categories={categories} />
+          <Main posts={posts} />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default HomePage;
